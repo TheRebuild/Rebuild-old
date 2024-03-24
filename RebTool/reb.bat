002edc927aac2307e886231b088660eefde903a2
snaky1a <snaky1a@outlook.com>
2024-03-24 21:53:18 +0200

    Updated RebTool

--- RebTool/reb.bat
+++ RebTool/reb.bat
@@ -2,9 +2,11 @@
 setlocal EnableDelayedExpansion
 
 set "CURRENT_DIR=%~dp0"
-set "CURRENT_VERSION=0.0.1" 
+set "CURRENT_VERSION=0.0.2" 
 :: 0 - no connection ; 1 - have connection 
 set "Internet=0"
+
+title RebTool ^| version: %CURRENT_VERSION%
 
 :main_menu 
 echo ===========================
@@ -12,8 +14,10 @@ echo ^| 1. Windows Defender     ^|
 echo ^| 2. exit                 ^| 
 echo ===========================  
 
-set /p choice="Enter your choice: "
-if %choice%==1 ( goto defender_menu ) else ( exit )
+rem set /p choice="Enter your choice: "
+choice /C:12 /N /M "Enter your choice: "
+if errorlevel 2 goto :EOF
+if errorlevel 1 goto defender_menu
 
 
 :defender_menu 
@@ -45,15 +49,18 @@ if "%DEFENDER_CURRENT_STATUS%"=="Enabled" (
 echo ^| 2. Go to main menu            ^| 
 echo =================================  
 
-set /p defender_choice="Enter your choice: "  
+rem set /p defender_choice="Enter your choice: "  
+choice /C:12 /N /M "Enter your choice: "
 
-if !defender_choice!==1 ( goto switch_defender ) else if !defender_choice!==2 ( goto :main_menu )   
+if errorlevel 2 goto :main_menu
+if errorlevel 1 goto switch_defender
 
 :switch_defender 
 if "%DEFENDER_CURRENT_STATUS%"=="Enabled" (
-   call %CURRENT_DIR%\utils\DisableDefender.bat
+   start /b /wait "disable defender" "%CURRENT_DIR%\utils\DisableDefender.bat >NUL 2>NUL"
 ) else if "%DEFENDER_CURRENT_STATUS%"=="Disabled" (
-   call %CURRENT_DIR%\utils\EnableDefender.bat
+   start /b /wait "enable defender" "%CURRENT_DIR%\utils\EnableDefender.bat >NUL 2>NUL"
 )
 
-pause
+echo Press any key to exit...
+pause >NUL 2>NUL
